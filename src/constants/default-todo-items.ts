93642044ac075e90a5c5787230860c38ae74c215
VladCuciureanu <vlad.cuciureanu@pm.me>
2023-03-29 18:15:48 +0300

    feat: Colorful default items

--- src/constants/default-todo-items.ts
+++ src/constants/default-todo-items.ts
@@ -2,16 +2,23 @@ import { TodoItem } from "@/types/todo-items";
 
 const DefaultTodoItems: TodoItem[] = [
   {
+    id: "8ec10364-d02e-4bc8-a2d4-6c30dca28fa3",
+    checked: false,
+    content: "User-centric scalable portal",
+  },
+  {
     id: "9b54572f-de4a-4235-b82f-71dc3d35f0f9",
     checked: true,
     content:
       "If we generate the protocol, we can get to the SMS microchip through the wireless XSS bandwidth!",
+    color: "red4",
   },
   {
     id: "0f2bef70-905e-4764-a353-a42d76360389",
     checked: false,
     content: "We need to program the neural DNS microchip!",
     parent: "9b54572f-de4a-4235-b82f-71dc3d35f0f9",
+    color: "orange4",
   },
   {
     id: "92bb2050-43f7-46d1-820e-ef7dbf0534c9",
@@ -19,6 +26,7 @@ const DefaultTodoItems: TodoItem[] = [
     content:
       "The UDP microchip is down, synthesize the solid state application so we can index the JSON system!",
     parent: "9b54572f-de4a-4235-b82f-71dc3d35f0f9",
+    color: "yellow4",
   },
   {
     id: "92bb2050-43f7-46d1-820e-ef7dbf0534cd",
@@ -26,17 +34,20 @@ const DefaultTodoItems: TodoItem[] = [
     content:
       "I'll generate the auxiliary UDP system, that should transmitter the PNG microchip!",
     parent: "92bb2050-43f7-46d1-820e-ef7dbf0534c9",
+    color: "green4",
   },
   {
     id: "85d78829-ae35-48a3-a250-11781c991e16",
     checked: false,
     content:
       "I'll synthesize the multi-byte PCI hard drive, that should matrix the HTTP program!",
+    color: "blue4",
   },
   {
     id: "adb05e07-a422-4a24-a5a5-53596c3c33ed",
     checked: true,
     content: "We need to index the optical IB monitor!",
+    color: "purple4",
   },
 ];
 
