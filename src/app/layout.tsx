648b3cf96c897d9b90316ea557e679340a4f37b7
VladCuciureanu <vlad.cuciureanu@pm.me>
2023-03-30 17:29:37 +0300

    feat: Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={styles.Body}>
         <Providers>
           <div className={styles.Container}>
